39398d13418dc1150ec632b2e3eebbf588b6d070
Derik Evangelista <devangelista@pivotal.io>
2016-03-15 16:04:49 +0000

    remove firehose logs
    operators don't care about that
    
    [#114679107]
    
    Signed-off-by: Bence Monus <bmonus@pivotal.io>

--- src/logsearch-config/test/logstash-filters/default-spec.rb
+++ src/logsearch-config/test/logstash-filters/default-spec.rb
@@ -12,18 +12,6 @@ describe "The combined parsing rules" do
     CONFIG
   end
 
-  describe 'when parsing firehose logs' do
-    when_parsing_log(
-      "@type" => "syslog",
-      "@message" => '<6>2015-03-17T01:24:23Z jumpbox.xxxxxxx.com doppler[6375]: {"cf_app_id":"b732c465-0536-4014-b922-165eb38857b2","level":"info","message_type":"OUT","msg":"Stopped app instance (index 0) with guid b732c465-0536-4014-b922-165eb38857b2","source_instance":"7","source_type":"DEA","time":"2015-03-17T01:24:23Z"}'
-    ) do
-
-      it "adds the firehose tag" do
-        expect(subject["tags"]).to include 'firehose'
-      end
-    end
-  end
-
   describe "when parsing UAA audit logs" do
     when_parsing_log(
       "@type" => "syslog",
