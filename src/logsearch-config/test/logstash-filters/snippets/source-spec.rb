e5f0b4cf5a33dab5f0dc5787a53f8829b829560a
Bence Monus <bmonus@pivotal.io>
2016-04-15 10:48:52 +0100

    restrict filter to certain logs
    [#114479147]
    
    Signed-off-by: Alex Blease <ablease@pivotal.io>

--- src/logsearch-config/test/logstash-filters/snippets/source-spec.rb
+++ src/logsearch-config/test/logstash-filters/snippets/source-spec.rb
@@ -50,4 +50,18 @@ describe "Extracting @source information" do
 
     end
   end
+
+  context "when log does not have job and index params" do
+    when_parsing_log(
+      "@type" => "syslog",
+      "syslog_program" => "vcap.cloud_controller_ng",
+      "syslog_hostname" => "10.10.81.5",
+      "@message" => '{"timestamp":1458648262.0071645,"message":"Statsd: cc.requests.outstanding:-1|c\ncc.requests.completed:1|c\ncc.http_status.2XX:1|c","log_level":"debug","source":"statsd.client","data":{},"thread_id":47073447296880,"fiber_id":47073444675740,"process_id":3255,"file":"/var/vcap/packages/cloud_controller_ng/cloud_controller_ng/vendor/bundle/ruby/2.2.0/gems/statsd-ruby-1.2.1/lib/statsd.rb","lineno":254,"method":"send_to_socket"}'
+    ) do
+
+      it "does not add the source tag" do
+        expect(subject["tags"]).to be_nil
+      end
+    end
+  end
 end
