0329ee3f6b1a09d30a760d78bc02b54d5af49b9a
Ada Solcan <asolcan@pivotal.io>
2016-05-26 10:56:11 +0100

    add hostname to backup archives
    to make them unique
    
    [#119618365]
    
    Signed-off-by: Bence Monus <bmonus@pivotal.io>

--- src/logs-backup/before_backup.sh
+++ src/logs-backup/before_backup.sh
@@ -5,6 +5,7 @@ DESTINATION="/var/vcap/store/parser/logs-to-be-archived"
 mkdir -p "$DESTINATION"
 
 for archive in $(ls -1 -t "$ORIGIN" | awk 'NR > 1'); do
-  mv "$ORIGIN/$archive" "$DESTINATION"
+  base=$(basename $archive .log.gz)
+  mv "$ORIGIN/$archive" "$DESTINATION/$base-$(hostname).log.gz"
 done
 
