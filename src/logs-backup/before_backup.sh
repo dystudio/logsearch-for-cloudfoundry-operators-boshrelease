0e88764045434171b49d320c9dbafe8faf088109
Alex Blease <ablease@pivotal.io>
2016-06-03 14:55:10 +0100

    Re-implement the before_backup script
    - Timestamp the archive file with the current system time, not the event
    time as was done by logstash
    - Add an id to timestamped archives to prevent conflicts in naming
    when writing to s3
    
    [#120640101]
    
    Signed-off-by: Ada Solcan <asolcan@pivotal.io>

--- src/logs-backup/before_backup.sh
+++ src/logs-backup/before_backup.sh
@@ -4,8 +4,12 @@ ORIGIN="/var/vcap/store/parser/archive"
 DESTINATION="/var/vcap/store/parser/logs-to-be-archived"
 mkdir -p "$DESTINATION"
 
+# Aim with this section
+# Create files that contain some identifier to the parser that created the file (hostname)
+# Create files that prevent incoming old data overwriting an archive in s3 (random-string)
 for archive in $(ls -1 -t "$ORIGIN" | awk 'NR > 1'); do
-  base=$(basename $archive .log.gz)
-  mv "$ORIGIN/$archive" "$DESTINATION/$base-$(hostname).log.gz"
+  timestamp=$(date +"%Y.%m.%d.%H")
+  id=$(cat /dev/urandom | tr -cd [:alnum:] | head -c 8)
+  mv "$ORIGIN/$archive" "$DESTINATION/$timestamp-$(hostname)-$id.log.gz"
 done
 
