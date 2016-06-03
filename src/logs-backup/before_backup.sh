7fd997136ff2e4c3017400e1f10e070f37f1e14d
Ada Solcan <asolcan@pivotal.io>
2016-06-03 16:33:08 +0100

    Use the Logstash timestamp for archives and change the id to be the md5 of the archive
    [#120640101]
    
    Signed-off-by: Alex Blease <ablease@pivotal.io>

--- src/logs-backup/before_backup.sh
+++ src/logs-backup/before_backup.sh
@@ -8,8 +8,8 @@ mkdir -p "$DESTINATION"
 # Create files that contain some identifier to the parser that created the file (hostname)
 # Create files that prevent incoming old data overwriting an archive in s3 (random-string)
 for archive in $(ls -1 -t "$ORIGIN" | awk 'NR > 1'); do
-  timestamp=$(date +"%Y.%m.%d.%H")
-  id=$(cat /dev/urandom | tr -cd [:alnum:] | head -c 8)
-  mv "$ORIGIN/$archive" "$DESTINATION/$timestamp-$(hostname)-$id.log.gz"
+  base=$(basename $archive .log.gz)
+  md5=$(md5sum $archive | awk '{print $1}')
+  mv "$ORIGIN/$archive" "$DESTINATION/$base-$(hostname)-$md5.log.gz"
 done
 
