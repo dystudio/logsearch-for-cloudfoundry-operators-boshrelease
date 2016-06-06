990314252a34ddae73743b9a5a75809be5e9be24
Derik Evangelista <devangelista@pivotal.io>
2016-06-06 15:21:52 +0100

    Calculate md5 using the file full path
    [#120640101]
    
    Signed-off-by: Ada Solcan <asolcan@pivotal.io>

--- src/logs-backup/before_backup.sh
+++ src/logs-backup/before_backup.sh
@@ -9,7 +9,7 @@ mkdir -p "$DESTINATION"
 # Create files that prevent incoming old data overwriting an archive in s3 (random-string)
 for archive in $(ls -1 -t "$ORIGIN" | awk 'NR > 1'); do
   base=$(basename $archive .log.gz)
-  md5=$(md5sum $archive | awk '{print $1}')
+  md5=$(md5sum "$ORIGIN/$archive" | awk '{print $1}')
   mv "$ORIGIN/$archive" "$DESTINATION/$base-$(hostname)-$md5.log.gz"
 done
 
