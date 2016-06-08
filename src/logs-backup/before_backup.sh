ad69536f41f4b5185fd1b736b0642e91feea9438
Alex Blease <ablease@pivotal.io>
2016-06-08 12:02:50 +0100

    Only use the md5 for identifying log archives
    [#121061535]
    
    Signed-off-by: Derik Evangelista <devangelista@pivotal.io>

--- src/logs-backup/before_backup.sh
+++ src/logs-backup/before_backup.sh
@@ -5,11 +5,10 @@ DESTINATION="/var/vcap/store/parser/logs-to-be-archived"
 mkdir -p "$DESTINATION"
 
 # Aim with this section
-# Create files that contain some identifier to the parser that created the file (hostname)
-# Create files that prevent incoming old data overwriting an archive in s3 (random-string)
+# Create files that prevent incoming old data overwriting an archive in s3 (file md5)
 for archive in $(ls -1 -t "$ORIGIN" | awk 'NR > 1'); do
   base=$(basename $archive .log.gz)
   md5=$(md5sum "$ORIGIN/$archive" | awk '{print $1}')
-  mv "$ORIGIN/$archive" "$DESTINATION/$base-$(hostname)-$md5.log.gz"
+  mv "$ORIGIN/$archive" "$DESTINATION/$base-$md5.log.gz"
 done
 
