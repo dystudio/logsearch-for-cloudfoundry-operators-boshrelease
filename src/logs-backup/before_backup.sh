4dafe06caa8781e1fe62a39b7e15b86b12384b0f
Derik Evangelista <devangelista@pivotal.io>
2016-06-10 15:23:34 +0100

    Provide a range of times that can delay the logs-backup job
    [#121300031]
    
    Signed-off-by: Alex Blease <ablease@pivotal.io>

--- src/logs-backup/before_backup.sh
+++ src/logs-backup/before_backup.sh
@@ -2,7 +2,12 @@
 
 ORIGIN="/var/vcap/store/parser/archive"
 DESTINATION="/var/vcap/store/parser/logs-to-be-archived"
+DELAY=$((${RANDOM:0:3} + 30))
 mkdir -p "$DESTINATION"
+
+#Delay the archiving job by at least 30 seconds, and anywhere up to 1029 seconds
+echo 'Sleeping for $DELAY seconds'
+sleep $DELAY
 
 # Aim with this section
 # Create files that prevent incoming old data overwriting an archive in s3 (file md5)
