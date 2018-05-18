0b07ebf8046ecb6cd93ec83540bb0bf6f9e189a9
eve quintana <jquintana@pivotal.io>
2018-05-18 16:24:54 -0700

    parse route-emitter logs for annotation
    [#157130798] Enable RFC3339-timestamped Diego logs on cloudops isolation segment and adjust logsearch parsing rules to parse them2
    
    Signed-off-by: Raina Masand <rmasand@pivotal.io>

--- src/logsearch-config/test/logstash-filters/snippets/diego-spec.rb
+++ src/logsearch-config/test/logstash-filters/snippets/diego-spec.rb
@@ -43,6 +43,38 @@ describe "Diego component logs" do
     end
   end
 
+  describe "Route emitter log parsing rules" do
+    when_parsing_log(
+      '@source' => { 'program' => 'route_emitter' },
+      '@message' => '{"timestamp":"1466769728.520450354","source":"rep","message":"rep.executing-container-operation.starting","log_level":1,"data":{"container-guid":"6dcf9128-3762-4624-839f-0e640cec2ff1-6a9abb10-0757-4bd7-847b-623fb28b4661-9469e7e3-92b9-4305-4a75-882f1b04944d","session":"61"}}'
+    ) do
+
+      it "adds bosh nats tag" do
+        expect(subject["tags"]).to include "diego/json"
+      end
+
+      it "sets @timestamp" do
+        expect(subject["@timestamp"]).to eq Time.parse("2016-06-24T12:02:08.520Z")
+      end
+
+      it "sets @timestamp_ns" do
+        expect(subject["@timestamp_ns"]).to eq 450354
+      end
+
+      it "sets @level" do
+        expect(subject["@level"]).to eq "INFO"
+      end
+
+      it "parses the message as json" do
+        expect(subject["route_emitter"]["data"]["session"]).to eq "61"
+      end
+
+      it "sets @message" do
+        expect(subject["@message"]).to eq "rep.executing-container-operation.starting"
+      end
+    end
+  end
+
   describe "alternate formats" do
     context 'when log_level is set to a word' do
       when_parsing_log(
@@ -58,7 +90,7 @@ describe "Diego component logs" do
 
     context 'when level is set to a word' do
       when_parsing_log(
-        '@source' => { 'program' => 'rep' },
+        '@source' => { 'program' => 'route_emitter' },
         '@message' => '{"timestamp":"2016-06-24T05:02:08.520450354-07:00","source":"rep","message":"rep.executing-container-operation.starting","level":"DEBUG","data":{"container-guid":"6dcf9128-3762-4624-839f-0e640cec2ff1-6a9abb10-0757-4bd7-847b-623fb28b4661-9469e7e3-92b9-4305-4a75-882f1b04944d","session":"61"}}'
       ) do
 
@@ -70,7 +102,7 @@ describe "Diego component logs" do
 
     context 'when level is set to a number' do
       when_parsing_log(
-        '@source' => { 'program' => 'rep' },
+        '@source' => { 'program' => 'bbs' },
         '@message' => '{"timestamp":"2016-06-24T05:02:08.520450354-07:00","source":"rep","message":"rep.executing-container-operation.starting","level":"3","data":{"container-guid":"6dcf9128-3762-4624-839f-0e640cec2ff1-6a9abb10-0757-4bd7-847b-623fb28b4661-9469e7e3-92b9-4305-4a75-882f1b04944d","session":"61"}}'
       ) do
 
